ca3ef11ef79ce620b6482339051408871fb3c47f
Jaime <lcalmsky@gmail.com>
2021-08-10 23:59:18 +0900

    Add submodule

---
+++ simple-grpc-server/src/test/java/io/lcalmsky/grpc/server/SimpleGrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.grpc.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGrpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
